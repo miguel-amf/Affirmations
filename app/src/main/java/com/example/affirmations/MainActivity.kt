2d75de8aa2325b0718b74d90c061277152b39a8e
unknown <dev@miguelmontagner.com>
2022-07-26 16:40:15 +0100

    displaying all the affirmations on the screen

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var source = Datasource()
+
+        var textview = findViewById<TextView>(R.id.caixa_inicial)
+
+        var output = ""
+        for (aff in source.loadAffirmations()) {
+            output += resources.getString(aff.stringResourceId) + "\n"
+        }
+        textview.text = output
     }
 }
